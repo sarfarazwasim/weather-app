cde1c9b01a1ce0d008e8162cf6b9cbe0004c0582
sarfaraz.wasim <sarfaraz.wasim@gdn-commerce.com>
2023-06-20 12:08:11 +0530

    Added new font

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
 import filters from "./utils/filters";
+import '@/assets/scss/style.scss'
 
 const app = createApp(App);
 app.use(router).use(createPinia()).mount("#app");
