b2b7415948678ff2aef7230420515186c40273bd
sarfaraz.wasim <sarfaraz.wasim@gdn-commerce.com>
2023-06-23 15:10:51 +0530

    Rebuild app with minor changes

--- app/build/generated/source/buildConfig/debug/app/netlify/weather_web_app_by_sarfaraz/twa/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/app/netlify/weather_web_app_by_sarfaraz/twa/BuildConfig.java
@@ -7,6 +7,6 @@ public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
   public static final String APPLICATION_ID = "app.netlify.weather_web_app_by_sarfaraz.twa";
   public static final String BUILD_TYPE = "debug";
-  public static final int VERSION_CODE = 1;
-  public static final String VERSION_NAME = "1";
+  public static final int VERSION_CODE = 2;
+  public static final String VERSION_NAME = "2";
 }
